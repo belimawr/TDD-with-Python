9972453dbde8bc9ca58ae556e15a9d328652417e
Tiago Queiroz <contato@tiago.eti.br>
2016-04-06 23:46:30 -0300

    init TDD. Django working, functional_tests is a WIP

---
+++ functional_tests.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+# Edith has heard about a cool new online to-do app. She goes
+# to check out its homepage
+browser.get('http://localhost:8000')
+
+# She notices the page title and header mention to-do lists
+assert 'Django' in browser.title
